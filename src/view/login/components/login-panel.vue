106319020a2520feeb803ca045ecc03180f73bbd
seekHoo <hu@seekhoo.cn>
2023-04-04 17:29:54 +0800

    login-panel

--- src/view/login/components/login-panel.vue
+++ src/view/login/components/login-panel.vue
@@ -1,17 +1,55 @@
 <template>
 	<div class="login-panel">
 		<h1 class="title">后台管理系统</h1>
-		<div class="tabs">tabs</div>
+		<div class="tabs">
+			<el-tabs type="border-card" stretch v-model:model-value="activeName">
+				<el-tab-pane name="account">
+					<template #label>
+						<div class="label">
+							<el-icon><User /></el-icon>
+							<span class="text">账号登录</span>
+						</div>
+					</template>
+					<panelAccount />
+				</el-tab-pane>
+				<el-tab-pane label="手机登录" name="phone">
+					<template #label>
+						<div class="label">
+							<el-icon><Iphone /></el-icon>
+							<span class="text">手机登录</span>
+						</div>
+					</template>
+					<panelPhone />
+				</el-tab-pane>
+			</el-tabs>
+		</div>
 		<div class="controls">
 			<el-checkbox v-model="isKeepPwd" label="记住密码" size="large" />
 			<el-link type="primary">忘记密码</el-link>
 		</div>
-		<div class="login-btn">aaa</div>
+		<el-button
+			class="login-btn"
+			type="primary"
+			size="large"
+			@click="handleLoginBtnClick"
+		>
+			立即登录
+		</el-button>
 	</div>
 </template>
 
 <script setup lang="ts">
+import panelAccount from './panel-account.vue'
+import panelPhone from './panel-phone.vue'
 const isKeepPwd = ref(false)
+const activeName = ref('account')
+const handleLoginBtnClick = () => {
+	if (activeName.value === 'phone') {
+		console.log('phone is active')
+	} else {
+		console.log('account is active')
+	}
+}
 </script>
 
 <style scoped lang="less">
@@ -25,7 +63,7 @@ const isKeepPwd = ref(false)
 		margin-bottom: 15px;
 	}
 
-	.icon {
+	.label {
 		display: flex;
 		align-items: center;
 		justify-content: center;
@@ -35,7 +73,7 @@ const isKeepPwd = ref(false)
 		}
 	}
 
-	.control-account {
+	.controls {
 		margin-top: 12px;
 		display: flex;
 
---
+++ src/view/login/components/panel-account.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<el-form label-width="100px" style="max-width: 460px" label-position="left">
+			<el-form-item label="Name">
+				<el-input />
+			</el-form-item>
+			<el-form-item label="Activity zone">
+				<el-input />
+			</el-form-item>
+			<el-form-item label="Activity form">
+				<el-input />
+			</el-form-item>
+		</el-form>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>

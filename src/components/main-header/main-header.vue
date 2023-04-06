945988ed2b8db9711d466440b6ae90940b5e6357
seekHoo <hu@seekhoo.cn>
2023-04-06 21:38:57 +0800

    aside

---
+++ src/components/main-header/main-header.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="main-header">main-header</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped></style>
--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -1,22 +1,35 @@
 <template>
-	<div>
-		main
-		<el-button type="primary" size="default" @click="logout">
-			退出登录
-		</el-button>
+	<div class="main">
+		<el-container class="main-content">
+			<el-aside width="210px">
+				<main-menu />
+			</el-aside>
+			<el-container>
+				<el-header height="50px">
+					<main-headere />
+				</el-header>
+				<el-main>Main</el-main>
+			</el-container>
+		</el-container>
 	</div>
 </template>
 
-<script setup lang="ts">
-import { LOGIN_TOKEN } from '@/global/constants'
-import { localCache } from '@/utils/cache'
-const router = useRouter()
-const logout = () => {
-	// 删除token
-	localCache.removeCache(LOGIN_TOKEN)
-	// 跳转login
-	router.push('/login')
-}
-</script>
+<script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="less">
+.main {
+	height: 100%;
+	.main-content {
+		height: 100%;
+		.el-aside {
+			background-color: red;
+		}
+		.el-header {
+			background-color: goldenrod;
+		}
+		.el-main {
+			background-color: blueviolet;
+		}
+	}
+}
+</style>
